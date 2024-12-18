9d7c70c0eb9e2ea3d75aed3dad1d90e604d31736
Nicolas Toconas <ntoconas@lexim.solutions>
2024-12-18 18:14:45 -0300

    Agregando colors para la consola y usando variable para el puerto

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from "express"
 import router from "./router"
 import db from "./config/db"
+import colors from "colors"
 
 // Conectar a base de datos
 async function connectDB() {
     try{
         await db.authenticate()
         db.sync()
-        console.log("Conexion exitosa a la BD")
+        console.log(colors.blue("Conexion exitosa a la BD"))
     } catch(error) {
-        console.log(error)
-        console.log("Hubo un error al conectar a la BD")
+        //console.log(error)
+        console.log(colors.red.bold("Hubo un error al conectar a la BD"))
     }
 }
 
