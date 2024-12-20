d2c4b110fc329b06e047a0216447e0d75da2f527
Nicolas Toconas <ntoconas@lexim.solutions>
2024-12-19 21:27:34 -0300

    Handler para updateProduct desde la api

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -39,3 +39,22 @@ export const createProduct = async (req : Request, res : Response) => {
         console.log(error)
     }
 }
+
+export const updateProduct = async  (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product) {
+            return res.status(404).json({
+                error: "Producto no encontrado"
+            })
+        }
+
+        await product.update(req.body)
+        await product.save()
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
+}
