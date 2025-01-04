aa5ed260ff1afcbb876aee02c7e8dbaa94aadfbe
Nicolas Toconas <ntoconas@lexim.solutions>
2025-01-03 21:25:16 -0300

    Tests para POST GET PUT Y DELETE

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -1,7 +1,7 @@
 import supertest from "supertest"
 import server from "../../server"
 
-describe("Post /api/products", () => {
+describe("POST /api/products", () => {
 
     it("Should display validation errors", async () =>{
         const response = await supertest(server).post("/api/products").send({})
@@ -42,5 +42,153 @@ describe("Post /api/products", () => {
         expect(response.status).not.toBe(400)
         expect(response.status).not.toBe(404)
         expect(response.body).not.toHaveProperty("errors")
+    })
+})
+
+describe("GET /api/products", () => {
+    it("should check if api/products url exists", async () => {
+        const response = await supertest(server).get("/api/products")
+        expect(response.status).not.toBe(404)
+    })
+    it("GET a JSON response with products", async() => {
+        const response = await supertest(server).get("/api/products")
+        expect(response.status).toBe(200)
+        expect(response.headers["content-type"]).toMatch(/json/)
+        expect(response.body).toHaveProperty("data")
+        expect(response.body.data).toHaveLength(1)
+        expect(response.body).not.toHaveProperty("errors")
+    })
+})
+
+describe('GET /api/products/:id', () => {
+    it('Should return a 404 response for a non-existent product', async () => {
+        const productId = 2000
+        const response = await supertest(server).get(`/api/products/${productId}`)
+        expect(response.status).toBe(404)
+        expect(response.body).toHaveProperty('error')
+        expect(response.body.error).toBe('Producto no encontrado')
+    })
+
+    it('should check a valid ID in the URL', async () => {
+        const response = await supertest(server).get('/api/products/not-valid-url')
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('ID no válido')
+    })
+
+    it('get a JSON response for a single product', async () => {
+        const response = await supertest(server).get('/api/products/1')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('data')
+    })
+})
+
+describe('PUT /api/products/:id', () => {
+
+    it('should check a valid ID in the URL', async () => {
+        const response = await supertest(server)
+                            .put('/api/products/not-valid-url')
+                            .send({
+                                name: "Monitor Curvo",
+                                availability: true,
+                                price : 300,
+                            })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('ID no válido')
+    })
+
+    it('should display validation error messages when updating a product', async() => {
+        const response = await supertest(server).put('/api/products/1').send({})
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toBeTruthy()
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('data')
+    }) 
+
+    it('should validate that the price is greater than 0', async() => {
+        const response = await supertest(server)
+                                .put('/api/products/1')
+                                .send({
+                                    name: "Monitor Curvo",
+                                    availability: true,
+                                    price : 0
+                                })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toBeTruthy()
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('Precio no válido')
+
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('data')
+    }) 
+
+    it('should return a 404 response for a non-existent product', async() => {
+        const productId = 2000
+        const response = await supertest(server)
+                                .put(`/api/products/${productId}`)
+                                .send({
+                                    name: "Monitor Curvo",
+                                    availability: true,
+                                    price : 300
+                                })
+
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Producto no encontrado')
+
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('data')
+    }) 
+
+    it('should update an existing product with valid data', async() => {
+        const response = await supertest(server)
+                                .put(`/api/products/1`)
+                                .send({
+                                    name: "Monitor Curvo",
+                                    availability: true,
+                                    price : 300
+                                })
+
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('data')
+
+        expect(response.status).not.toBe(400)
+        expect(response.body).not.toHaveProperty('errors')
+    }) 
+    
+
+})
+
+describe('DELETE /api/products/:id', () => {
+    it('should check a valid ID', async () => {
+        const response = await supertest(server).delete('/api/products/not-valid')
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors[0].msg).toBe('ID no válido')
+    })
+
+    it('should return a 404 response for a non-existent product', async () => {
+        const productId = 2000
+        const response = await supertest(server).delete(`/api/products/${productId}`)
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Producto no encontrado')
+        expect(response.status).not.toBe(200)
+    })
+
+    it('should delete a product', async () => {
+        const response = await supertest(server).delete('/api/products/1')
+        expect(response.status).toBe(200)
+        expect(response.body.data).toBe("Producto Eliminado")
+
+        expect(response.status).not.toBe(404)
+        expect(response.status).not.toBe(400)
     })
 })
