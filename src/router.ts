3b3cc42011fcac2da44b27f8bfafe8e7f37d1e54
Nicolas Toconas <ntoconas@lexim.solutions>
2025-01-05 20:44:53 -0300

    Agregada documentacion de products schema en swagger

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,32 @@ import { handleInputErrors } from "./middleware"
 
 const router = Router()
 
-//Routing
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: The Product ID
+ *                      example: 1
+ *                  name: 
+ *                      type: string
+ *                      description: The Product name
+ *                      example: Monitor Curvo de 49 Pulgadas
+ *                  price:
+ *                      type: number
+ *                      description: The Product price
+ *                      example: 300
+ *                  availability:
+ *                      type: boolean
+ *                      description: The Product availability
+ *                      example: true
+ */
+
+
 router.get("/", getProducts)
 
 router.get("/:id", 
